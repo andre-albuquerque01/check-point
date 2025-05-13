d6f0b2195b25eb5dbe169fbd68f119742c39a608
andre <andrealbuquerque2001@gmail.com>
2025-05-13 18:04:04 -0300

    feat: create model

---
+++ Api/app/Models/CheckIns.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+
+class CheckIns extends Model
+{
+    use HasUlids;
+
+    protected $table = "check_ins";
+    
+    protected $fillable = [
+        'check_in_time',
+        'check_out_time',
+        'check_date',
+        'user_id',
+    ];
+}
