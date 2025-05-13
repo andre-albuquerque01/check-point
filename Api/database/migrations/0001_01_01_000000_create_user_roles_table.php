71c56ad5ff9ae82659098b226f3502833abd94c1
andre <andrealbuquerque2001@gmail.com>
2025-05-13 18:03:51 -0300

    feat: create migration

---
+++ Api/database/migrations/0001_01_01_000000_create_user_roles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->ulid()->primary();
+            $table->string('role')->comment('O cargo/papel que tem na aplicação');
+            $table->string('description')->comment('Descrição do cargo/papel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_roles');
+    }
+};
