6b4e46baa1b6cce70ca54c6e183294ddc1e26940
andre <andrealbuquerque2001@gmail.com>
2025-05-13 18:05:47 -0300

    feat: update model

--- Api/app/Models/User.php
+++ Api/app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -48,7 +47,16 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'role' => UserRole::class,
         ];
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(UserRole::class, 'role_id');
+    }
+
+    public function checkIns()
+    {
+        return $this->hasMany(CheckIns::class);
     }
 }
