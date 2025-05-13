72fda82caf5f8776605741c8520c90de6f813b96
andre <andrealbuquerque2001@gmail.com>
2025-05-13 18:05:44 -0300

    feat: update model

--- Api/app/Models/CheckIns.php
+++ Api/app/Models/CheckIns.php
@@ -10,11 +10,16 @@ class CheckIns extends Model
     use HasUlids;
 
     protected $table = "check_ins";
-    
+
     protected $fillable = [
         'check_in_time',
         'check_out_time',
         'check_date',
         'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
