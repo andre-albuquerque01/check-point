7a99beae801e30014f62bed717804e2fe2455c01
andre <andrealbuquerque2001@gmail.com>
2025-05-13 18:03:55 -0300

    feat: create migration

--- Api/database/migrations/0001_01_01_000000_create_users_table.php
+++ Api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->ulid()->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignUlid('role_id')->references('id')->on('user_roles')->onDelete("cascade")->onUpdate("cascade");
+            $table->softDeletes();
             $table->timestamps();
         });
 
