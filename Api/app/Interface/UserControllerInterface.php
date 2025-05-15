45c02af21d8f04bc511e60fc4c7a3416b386ebc1
andre <andrealbuquerque2001@gmail.com>
2025-05-15 17:59:49 -0300

    feat: create interface

---
+++ Api/app/Interface/UserControllerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interface;
+
+use App\Http\Requests\AuthRequest;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UserUpdatePasswordRequest;
+use App\Http\Requests\UserUpdateRoleRequest;
+
+interface UserControllerInterface
+{
+    public function login(AuthRequest $request);
+    public function store(UserRequest $request);
+    public function show();
+    public function update(UserRequest $request);
+    public function updatePassword(UserUpdatePasswordRequest $request);
+    public function updateRoleUser(UserUpdateRoleRequest $request, string $id);
+    public function logout();
+}
