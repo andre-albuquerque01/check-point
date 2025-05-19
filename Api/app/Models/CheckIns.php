756660a5b2d85e85af711175ec4585e8feef8ceb
andre <andrealbuquerque2001@gmail.com>
2025-05-19 11:16:24 -0300

    feat: update model

--- Api/app/Models/CheckIns.php
+++ Api/app/Models/CheckIns.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CheckIns extends Model
 {
-    use HasUlids;
+    use HasFactory, HasUlids;
 
     protected $table = "check_ins";
 
