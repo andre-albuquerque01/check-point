4828a4edcaa10fc35611462c9c16ed7fb053c14d
Andre <andrealbuquerque2001@gmail.com>
2025-05-20 15:52:48 -0300

    feat: update request

--- Api/app/Http/Requests/RoleRequest.php
+++ Api/app/Http/Requests/RoleRequest.php
@@ -11,7 +11,7 @@ class RoleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +21,36 @@ class RoleRequest extends FormRequest
      */
     public function rules(): array
     {
+        $rules = [
+            "role" => "required|min:3|max:120|regex:/^[^<>]*$/",
+            "description" => "required|min:3|max:120|regex:/^[^<>]*$/",
+        ];
+
+        if ($this->method() == 'PATCH' || $this->method() == 'PUT') {
+            $rules["role"] = [
+                "nullable",
+            ];
+            $rules["description"] = [
+                'nullable',
+            ];
+        }
+
+        return $rules;
+    }
+
+    public function messages()
+    {
         return [
-            //
+            "role.required" => "O cargo é obrigatório.",
+            "role.min" => "O cargo deve ter pelo menos 3 caracteres.",
+            "role.max" => "O cargo não pode ter mais de 120 caracteres.",
+            "role.regex" => "O cargo contém caracteres inválidos.",
+
+            "description.required" => "A descrição é obrigatório.",
+            "description.min" => "A descrição deve ter pelo menos 3 caracteres.",
+            "description.max" => "A descrição não pode ter mais de 120 caracteres.",
+            "description.regex" => "A descrição contém caracteres inválidos.",
+
         ];
     }
 }
